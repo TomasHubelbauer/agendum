71b152fcec7ba534d88b60e6966e84c5c1a6511f
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-03 21:26:02 +0100

    Cache the application files

--- worker.js
+++ worker.js
@@ -1,4 +1,8 @@
 self.addEventListener('install', async event => {
   const cache = await caches.open('agendum');
-  console.log('Service worker has been installed.', cache);
+  cache.addAll(['index.html', 'index.js', 'index.css']);
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request));
 });
