21b0cb923d17fe7c162d4905fdd143c200c1e874
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-03 10:57:22 +0100

    Implement adding and iterating items

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 window.addEventListener('load', _ => {
-  alert('Loaded');
+  const editorInput = document.querySelector('#editorInput');
+  const submitButton = document.querySelector('#submitButton');
+  const itemsUl = document.querySelector('#itemsUl');
+
+  submitButton.addEventListener('click', _ => {
+    if (editorInput.value === '') {
+      return;
+    }
+    
+    // TODO: Calculate +1 instead of `length` so that keys are always-growing even after implementing deletion
+    localStorage.setItem(localStorage.length, editorInput.value);
+    render();
+  });
+  
+  function render() {
+    itemsUl.innerHTML = '';
+    Object.keys(localStorage).forEach(key => {
+      const itemLi = document.createElement('li');
+      itemLi.textContent = localStorage.getItem(key);
+      itemsUl.appendChild(itemLi);
+    });
+  }
+  
+  render();
 });
