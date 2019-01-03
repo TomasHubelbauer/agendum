61e740e89dd736947cd4d7a401284e85550fdd70
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-03 11:01:31 +0100

    Submit on Enter key press as well

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ window.addEventListener('load', _ => {
   const itemsUl = document.querySelector('#itemsUl');
 
   submitButton.addEventListener('click', _ => {
-    if (editorInput.value === '') {
+    create(editorInput.value);
+  });
+  
+  editorInput.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+      create(editorInput.value);
+    }
+  });
+  
+  function create(text) {
+    if (!text) {
       return;
     }
     
     // TODO: Calculate +1 instead of `length` so that keys are always-growing even after implementing deletion
     localStorage.setItem(localStorage.length, editorInput.value);
     render();
-  });
+  }
   
   function render() {
     itemsUl.innerHTML = '';
