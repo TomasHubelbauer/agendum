85e8f0db7b34ed6e68f616e0f4abd803140227f3
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-07 20:01:43 +0100

    Place the screenshot to screenshots directory for artifact upload

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ async function ui() {
   const page = pages[0];
   // TODO: Go to the sources instead - but need to serve first, `file:///` will have different behavior
   await page.goto('https://agendum.today/');
-  await page.screenshot({path: 'test.png'});
+  await page.screenshot({path: 'screenshots/test.png'});
   // TODO: Upload screenshots as build artifacts from UI tests
   // TODO: Look into Puppeteer giving jUnit results: https://www.eliostruyf.com/setting-up-puppeteer-to-run-on-azure-devops-for-your-automated-ui-tests/
   console.log('Puppeteer');
