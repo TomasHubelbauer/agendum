90bf156148f1c0768878f1d058603f6e8e65542b
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-07 20:04:44 +0100

    Use console.error instead of throw and create screenshots directory

--- test.js
+++ test.js
@@ -1,18 +1,21 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
-async function ui() {
-  const browser = await puppeteer.launch();
-  const pages = await browser.pages();
-  const page = pages[0];
-  // TODO: Go to the sources instead - but need to serve first, `file:///` will have different behavior
-  await page.goto('https://agendum.today/');
-  await page.screenshot({path: 'screenshots/test.png'});
-  // TODO: Upload screenshots as build artifacts from UI tests
-  // TODO: Look into Puppeteer giving jUnit results: https://www.eliostruyf.com/setting-up-puppeteer-to-run-on-azure-devops-for-your-automated-ui-tests/
-  console.log('Puppeteer');
-  await browser.close();
-};
+fs.mkdirSync('screenshots');
 
-ui().catch(error => {
-  throw error;
-});
+void async function() {
+  try {
+    const browser = await puppeteer.launch();
+    const pages = await browser.pages();
+    const page = pages[0];
+    // TODO: Go to the sources instead - but need to serve first, `file:///` will have different behavior
+    await page.goto('https://agendum.today/');
+    await page.screenshot({path: 'screenshots/test.png'});
+    // TODO: Upload screenshots as build artifacts from UI tests
+    // TODO: Look into Puppeteer giving jUnit results: https://www.eliostruyf.com/setting-up-puppeteer-to-run-on-azure-devops-for-your-automated-ui-tests/
+    console.log('Puppeteer');
+    await browser.close();
+  } catch (error) {
+    console.error(error.message);
+  }
+}()
