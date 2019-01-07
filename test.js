5d0107eff60fc235566806f67121203e73de4ec3
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-07 19:50:54 +0100

    Set up a basic Puppeteer rig

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 const puppeteer = require('puppeteer');
+
+void async function() {
+  const browser = await puppeteer.launch();
+  const pages = await browser.pages();
+  const page = pages[0];
+  // TODO: Go to the sources instead - but need to serve first, `file:///` will have different behavior
+  await page.goto('https://agendum.today/');
+  // TODO: Upload screenshots as build artifacts from UI tests
+  // TODO: Look into Puppeteer giving jUnit results: https://www.eliostruyf.com/setting-up-puppeteer-to-run-on-azure-devops-for-your-automated-ui-tests/
+  console.log('Puppeteer');
+  await browser.close();
+}()
