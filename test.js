9920b79cb8d79cac54ec3d2899fb38459847434e
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-07 20:08:20 +0100

    Load the local file instead of the deployed site

--- test.js
+++ test.js
@@ -8,12 +8,10 @@ void async function() {
     const browser = await puppeteer.launch();
     const pages = await browser.pages();
     const page = pages[0];
-    // TODO: Go to the sources instead - but need to serve first, `file:///` will have different behavior
-    await page.goto('https://agendum.today/');
+    // TODO: Serve the files on localhost and use that, the `file:///` protocol will have different behavior
+    await page.goto('index.html');
     await page.screenshot({path: 'screenshots/test.png'});
     // TODO: Upload screenshots as build artifacts from UI tests
-    // TODO: Look into Puppeteer giving jUnit results: https://www.eliostruyf.com/setting-up-puppeteer-to-run-on-azure-devops-for-your-automated-ui-tests/
-    console.log('Puppeteer');
     await browser.close();
   } catch (error) {
     console.error(error.message);
