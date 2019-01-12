03f40922f8b81625b0987ae06b16c4b142f81429
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-12 20:51:10 +0100

    Get IDs from getIds

--- src/getItems.js
+++ src/getItems.js
@@ -1,5 +1,7 @@
-export default function* getItems(ids) {
-  for (let id of ids) {
+import getIds from './getIds.js';
+
+export default function* getItems() {
+  for (let id of getIds()) {
     const item = JSON.parse(localStorage.getItem(id.toString()));
     item.id = id;
     yield item;
