e2a91339756361b9e743976b8cdee28b267e17a9
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-12 20:49:58 +0100

    Get items from getItems()

--- src/getScheduledItems.js
+++ src/getScheduledItems.js
@@ -1,5 +1,7 @@
-export default function* getScheduledItems(items) {
-  for (let item of items) {
+import getItems from './getItems.js';
+
+export default function* getScheduledItems() {
+  for (let item of getItems()) {
     // TODO: Validate `notBeforeDate`
     if (item.archivedDate === undefined && item.notBeforeDate !== undefined) {
       yield item;
