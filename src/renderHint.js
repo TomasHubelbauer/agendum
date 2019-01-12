21727a3c430d3a520173332b8a660a0b67e503b0
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-12 21:17:11 +0100

    Clear the target until this is rendered with Fragment

--- src/renderHint.js
+++ src/renderHint.js
@@ -1,4 +1,6 @@
 export default function renderHint(useRichEditor) {
+  // TODO: Get rid of this hack once Fragments has support for keys and can properly reconcile sets
+  hintDiv.innerHTML = '';
   switch (navigator.platform) {
     case 'Win32': hintDiv.innerHTML = 'Press <kbd>Win+.</kbd> for emoji keyboard.'; break;
     case 'MacIntel': hintDiv.innerHTML = 'Press <kbd>Cmd+Ctrl+ </kbd> (space) for emoji keyboard.'; break;
