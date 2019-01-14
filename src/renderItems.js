267049390fc917794f9818f374b28d3431d3e5eb
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-14 11:03:35 +0100

    Update renderItems.js

--- src/renderItems.js
+++ src/renderItems.js
@@ -16,35 +16,47 @@ export default function renderItems(tab, onShowQueuedButtonClick, onShowSchedule
     button({ onclick: onShowQueuedButtonClick, disabled: tab === 'queued' ? 'disabled' : undefined }, 'Queued'),
     button({ onclick: onShowScheduledButtonClick, disabled: tab === 'scheduled' ? 'disabled' : undefined }, 'Scheduled'),
     button({ onclick: onShowArchivedButtonClick, disabled: tab === 'archived' ? 'disabled' : undefined }, 'Archived'),
+    tab === 'archived' && renderGroup('TODO: Group archived items by date', []),
     ...[...getTabItems(tab)].map((item, index, { length }) => {
-      const { title, description, createdDate, archivedDate, resolution, notBeforeDate, notAfterDate } = item;
-      return details(
-        { class: index % 2 === 0 ? 'even' : 'odd' },
-        summary(
-          span({ class: 'itemSpan' }, title),
-          button({ ['data-id']: item.id, onclick: onRenameButtonClick, title: `Rename '${title}'` }, 'Rename'),
-          tab === 'queued' && button({ ['data-id']: item.id, onclick: onArchiveButtonClick, title: `Archive '${title}'` }, 'Archive'),
-          tab === 'archived' && button({ ['data-id']: item.id, onclick: onReviveButtonClick, title: `Revive '${title}'` }, 'Revive'),
-          tab === 'archived' && button({ ['data-id']: item.id, onclick: onDeleteButtonClick, title: `Delete '${title}'` }, 'Delete'),
-          button({ ['data-id']: item.id, onclick: onMoveUpButtonClick, disabled: index === 0 ? 'disabled' : undefined, title: `Move '${title}' up` }, '▲'),
-          button({ ['data-id']: item.id, onclick: onMoveDownButtonClick, disabled: index === length - 1 ? 'disabled' : undefined, title: `Move '${title}' down` }, '▼'),
-        ),
-        ...(description || []).map(line => {
-          // Recognize lines that are a link as a whole
-          if ((line.startsWith('http://') || line.startsWith('https://')) && line.endsWith('/')) {
-            return div(a({ href: line, target: '_blank' }, line));
-          }
-
-          // TODO: Interpret as raw HTML to correctly render data URI image tags
-          return div(line);
-        }),
-        div(`ID: ${item.id}`),
-        createdDate && div('Created: ' + createdDate.toLocaleString()),
-        archivedDate && div('Archived: ' + archivedDate.toLocaleString()),
-        resolution && div('Resolution: ' + resolution),
-        notBeforeDate && div('Not before: ' + notBeforeDate.toLocaleString()),
-        notAfterDate && div('Not after: ' + notAfterDate.toLocaleString()),
-      );
+      return renderItem(item, index, length, onRenameButtonClick, onArchiveButtonClick, onReviveButtonClick, onDeleteButtonClick, onMoveUpButtonClick, onMoveDownButtonClick);
     })
+  );
+}
+
+function renderItem(item, index, length, onRenameButtonClick, onArchiveButtonClick, onReviveButtonClick, onDeleteButtonClick, onMoveUpButtonClick, onMoveDownButtonClick) {
+  const { title, description, createdDate, archivedDate, resolution, notBeforeDate, notAfterDate } = item;
+  return details(
+    { class: index % 2 === 0 ? 'even' : 'odd' },
+    summary(
+      span({ class: 'itemSpan' }, title),
+      button({ ['data-id']: item.id, onclick: onRenameButtonClick, title: `Rename '${title}'` }, 'Rename'),
+      tab === 'queued' && button({ ['data-id']: item.id, onclick: onArchiveButtonClick, title: `Archive '${title}'` }, 'Archive'),
+      tab === 'archived' && button({ ['data-id']: item.id, onclick: onReviveButtonClick, title: `Revive '${title}'` }, 'Revive'),
+      tab === 'archived' && button({ ['data-id']: item.id, onclick: onDeleteButtonClick, title: `Delete '${title}'` }, 'Delete'),
+      button({ ['data-id']: item.id, onclick: onMoveUpButtonClick, disabled: index === 0 ? 'disabled' : undefined, title: `Move '${title}' up` }, '▲'),
+      button({ ['data-id']: item.id, onclick: onMoveDownButtonClick, disabled: index === length - 1 ? 'disabled' : undefined, title: `Move '${title}' down` }, '▼'),
+    ),
+    ...(description || []).map(line => {
+      // Recognize lines that are a link as a whole
+      if ((line.startsWith('http://') || line.startsWith('https://')) && line.endsWith('/')) {
+        return div(a({ href: line, target: '_blank' }, line));
+      }
+
+      // TODO: Interpret as raw HTML to correctly render data URI image tags
+      return div(line);
+    }),
+    div(`ID: ${item.id}`),
+    createdDate && div('Created: ' + createdDate.toLocaleString()),
+    archivedDate && div('Archived: ' + archivedDate.toLocaleString()),
+    resolution && div('Resolution: ' + resolution),
+    notBeforeDate && div('Not before: ' + notBeforeDate.toLocaleString()),
+    notAfterDate && div('Not after: ' + notAfterDate.toLocaleString()),
+  );
+}
+
+function renderGroup(title, items) {
+  return details(
+    summary(title),
+    ...items,
   );
 }
