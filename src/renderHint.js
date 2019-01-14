56faa9eb9e6c2d989a881864c51af8fbb488dcd6
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-14 09:38:06 +0100

    Move the hint div loopup to the renderer

--- src/renderHint.js
+++ src/renderHint.js
@@ -1,4 +1,10 @@
 export default function renderHint(useRichEditor) {
+  /** @type{HTMLDivElement|null} */
+  const hintDiv = document.querySelector('#hintDiv');
+  if (hintDiv == null) {
+    throw new Error('Hint <div> not found');
+  }
+  
   // TODO: Get rid of this hack once Fragments has support for keys and can properly reconcile sets
   hintDiv.innerHTML = '';
   switch (navigator.platform) {
