744955a3a0dc65b9a6f4049d291b21f4b4ec21e9
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-14 10:31:47 +0100

    Parse out the dates while fetching the items

--- src/getItems.js
+++ src/getItems.js
@@ -4,6 +4,10 @@ export default function* getItems() {
   for (let id of getIds()) {
     const item = JSON.parse(localStorage.getItem(id.toString()));
     item.id = id;
+    item.createdDate && item.createdDate = new Date(item.createdDate);
+    item.archivedDate && item.archivedDate = new Date(item.archivedDate);
+    item.notBeforeDate && item.notBeforeDate = new Date(item.notBeforeDate);
+    item.notAfterDate && item.notAfterDate = new Date(item.notAfterDate);
     yield item;
   }
 }
