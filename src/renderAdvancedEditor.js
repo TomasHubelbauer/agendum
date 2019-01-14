b4c66891506a3196639a0a4b992bf6816575628b
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-14 09:43:06 +0100

    Implement a renderer for the advanced editor section

---
+++ src/renderAdvancedEditor.js
@@ -0,0 +1,40 @@
+export default function renderAdvancedEditor() {
+  /** @type{HTMLDivElement|null} */
+  const advancedEditorDiv = document.querySelector('#advancedEditorDiv');
+  if (advancedEditorDiv == null) {
+    throw new Error('Advanced editor <div> not found');
+  }
+  
+  // TODO: Get rid of this hack once Fragments has support for keys and can properly reconcile sets
+  advancedEditorDiv.innerHTML = '';
+
+  const advancedDetails = document.createElement('details');
+  advancedEditorDiv.appendChild(advancedDetails);
+
+  const advancedSummary = document.createElement('summary');
+  advancedSummary.textContent = 'Advanced';
+  advancedDetails.appendChild(advancedSummary);
+
+  const resolutionSelect = document.createElement('select');
+  advancedDetails.appendChild(resolutionSelect);
+
+  const archiveOption = document.createElement('option');
+  archiveOption.textContent = 'Archive';
+  resolutionSelect.appendChild(archiveOption);
+
+  const deleteOption = document.createElement('option');
+  deleteOption.textContent = 'Delete';
+  resolutionSelect.appendChild(deleteOption);
+
+  const graftOption = document.createElement('option');
+  graftOption.textContent = 'Graft';
+  resolutionSelect.appendChild(graftOption);
+
+  const notBeforeInput = document.createElement('input');
+  notBeforeInput.type = 'date';
+  advancedDetails.appendChild(notBeforeInput);
+
+  const notAfterInput = document.createElement('input');
+  notAfterInput.type = 'date';
+  advancedDetails.appendChild(notAfterInput);
+}
