fae8332226d7697402418ef7c1e266f3279f32ed
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-14 10:32:25 +0100

    Use parsed out dates

--- src/getScheduledItems.js
+++ src/getScheduledItems.js
@@ -2,7 +2,7 @@ import getItems from './getItems.js';
 
 export default function* getScheduledItems() {
   for (let item of getItems()) {
-    if (item.archivedDate === undefined && item.notBeforeDate !== undefined && new Date(item.notBeforeDate) > new Date()) {
+    if (item.archivedDate === undefined && item.notBeforeDate !== undefined && item.notBeforeDate > new Date()) {
       yield item;
     }
   }
