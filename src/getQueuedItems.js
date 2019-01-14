a72d028107dcc8b3722df9208a126e486971c0e7
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-14 09:35:51 +0100

    Display present and past scheduled items as well

--- src/getQueuedItems.js
+++ src/getQueuedItems.js
@@ -2,7 +2,7 @@ import getItems from './getItems.js';
 
 export default function* getQueuedItems() {
   for (let item of getItems()) {
-    if (item.archivedDate === undefined && item.notBeforeDate === undefined) {
+    if (item.archivedDate === undefined && item.notBeforeDate === undefined || item.notBeforeDate < new Date()) {
       yield item;
     }
   }
