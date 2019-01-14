52c18a7fd04705033c175e6f1babdfa2c992de04
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-14 16:58:22 +0100

    Start pulling out data-persistence related layer functions

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export function createDraft(title) {
+  const drafts = JSON.parse(localStorage.getItem('drafts') || '[]');
+  drafts.push({ title });
+  localStorage.setItem('drafts', JSON.stringify(drafts));
+}
+
+export function removeDraft(id) {
+  const drafts = JSON.parse(localStorage.getItem('drafts') || '[]');
+  const draft = drafts[index].title;
+  drafts.splice(index, 1);
+  localStorage.setItem('drafts', JSON.stringify(drafts));
+  return draft.title;
+}
+
+
+export function deleteDraft(index) {
+  const drafts = JSON.parse(localStorage.getItem('drafts') || '[]');
+  drafts.splice(index, 1);
+  localStorage.setItem('drafts', JSON.stringify(drafts));
+}
